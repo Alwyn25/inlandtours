89b87b4456c4453d2452a0c99e9da219bc9cb14f
Alwyn25 <99828232+Alwyn25@users.noreply.github.com>
2022-11-06 17:32:39 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,371 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/main.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo"><span>Inland Tours & Travels</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home">
+
+   <div class="swiper home-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide" style="background:url(images/home-bg-1.jpg) no-repeat">
+            <div class="content" id="page1">
+               <span>explore, discover, travel</span>
+               <h3>travel arround the world</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/home-bg-2.jpg) no-repeat">
+            <div class="content"id="page2" >
+               <span>explore, discover, travel</span>
+               <h3>discover the new places</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/home-bg-3.jpg) no-repeat">
+            <div class="content" id="page3">
+               <span>explore, discover, travel</span>
+               <h3>make your tour worthwhile</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+         
+      </div>
+
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+
+   </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- services section starts  -->
+
+<section class="services">
+
+   <h1 class="heading-title"> our services </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/icon-1.png" alt="">
+         <h3>adventure</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-2.png" alt="">
+         <h3>tour guide</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-3.png" alt="">
+         <h3>trekking</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-4.png" alt="">
+         <h3>camp fire</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-5.png" alt="">
+         <h3>off road</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-6.png" alt="">
+         <h3>camping</h3>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- services section ends -->
+
+<!-- home about section starts  -->
+
+<section class="home-about">
+
+   <div class="image">
+      <img src="images\about-image.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>about us</h3>
+      <p>Your Time Is Valuable. When You Pick Inland Tours & Travels, We Devote Time To Organising A First-Rate 
+         Experience Based On Our Local Knowledge, Allowing You To Spend Your Free Time As You See Fit. And Once You're
+         On The Road, Each Day Of Your Personalised Schedule Is Designed To Be Unforgettable.</p>
+      <a href="about.php" class="btn">read more</a>
+   </div>
+
+</section>
+
+<!-- home about section ends -->
+
+<!-- home domestic packages section starts  -->
+
+<section class="home-packages">
+
+   <h1 class="heading-title"> Domestic Packages </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images\lakshadweep.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Panoramic agatti island</h3></h3>
+            <p> Activities at Agatti comprise swimming in crystal clear waters, 
+               snorkelling and scuba diving Deep Sea Fishing, sailing, glass bottom
+               boat rides, water skiing and kayaking </i></p>
+            <a href="book.php" class="btn">explore</a>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/kashmir.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Explore the Frozen KASHMIR</h3>
+            <p>Why even think of Switzerland, when you have Kashmir. Yeah, that’s true! We all know how beautiful Kashmir is during winters. The views of snow-covered meadows,</p>
+            <a href="book.php" class="btn">explore</a>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+      
+      <div class="box">
+         <div class="image">
+            <img src="images/img-3.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Explore the beauty of KERALA</h3>
+            <p>Have you ever imagined what Paradise would look like? Where you can smell the essence of fresh air bouncing back from your cheeks. Loads of palm trees swaying all around you. </p>
+            <a href="book.php" class="btn">explore</a>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+      
+
+   </div>
+
+   <div class="load-more"> <a href="package.php#domesti" class="btn">load more</a> </div>
+
+</section>
+
+<!-- home domestic packages section ends -->
+
+
+<!-- home international packages section starts -->
+
+<section class="home-packages">
+
+   <h1 class="heading-title"> International Packages </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>OMAN GETAWAY</h3>
+            <p>Looking for an unforgettable tour in Oman? Look no further than our selection of tours specifically designed to show you the best that this amazing country has to offer!
+            explore the country's stunning landscapes or Traditional  life.
+            </p>
+            <a href="book.php" class="btn">explore</a>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/thailand.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>ESSENTIAL THAILAND</h3>
+            <p>Thailand is a Southeast Asian country known for its stunning beaches, lush jungles, and vibrant cities. From the bustling metropolis of Bangkok to the laid-back island of Phuket, there's something for everyone in Thailand.</p><br><br>
+            <a href="book.php" class="btn">explore</a>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+      
+      <div class="box">
+         <div class="image">
+            <img src="images\netherlands.webp" alt="">
+         </div>
+         <div class="content">
+            <h3>ESSENTIAL NETHERLANDS</h3>
+            <p>Netherlands is a beautiful country full of amazing sights and attractions. From the world-famous canals of Amsterdam to the picturesque windmills of Kinderdijk, there is so much to see and do in Netherlands.</p>
+            <a href="book.php" class="btn">explore</a>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"> <a href="package.php#international" class="btn">load more</a> </div>
+
+</section>
+
+<!-- home international packages section ends -->
+
+
+<!-- home offer section starts  -->
+
+<!-- <section class="home-offer">
+   <div class="content">
+      <h3>upto 50% off</h3>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure tempora assumenda, debitis aliquid nesciunt maiores quas! Magni cumque quaerat saepe!</p>
+      <a href="book.php" class="btn">book now</a>
+   </div>
+</section> -->
+
+<!-- home offer section ends -->
+
+<!-- map section starts -->
+<section class="map">
+            <div class="contact_us" id="address">
+                <div class="head">
+                    <h1>Contact Us</h1>
+                </div>
+                <div class="address" >
+                    <h3>
+                        Pallickal Houes,
+                        Palemad Post ,
+                        Edakkara,
+                        Nilambur,
+                        Malappuram,
+                        Kerala - 679331 <br> <br>
+                    </h3>
+                </div>
+                <div class="mapouter">
+                    <div class="gmap_canvas" >
+                        <iframe class="gmap_iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=11.393212,76.309944&amp;t=k&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" style="width:100%;height:300px;"></iframe>
+                        
+                    </div>
+                </div>
+            </div>
+            
+        </section>
+
+<!-- map section ends -->
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="./privacy.php#privacy"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="./terms.php#terms"><i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="tel:+91-9847449044"> <i class="fas fa-phone"></i> +91-9847449044 </a>
+         <a href="tel:+91-8848474994"> <i class="fas fa-phone"></i> +91-8848474994 </a>
+         <a href="mailto: info@inlandtours.in "> <i class="fas fa-envelope"></i>  Info@Inlandtours.In </a>
+         <a href="#"> <i class="fas fa-map"></i> Nilambur, india - 679331 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/profile.php?id=100086423054721"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="https://www.instagram.com/invites/contact/?i=msrdweo0egra&utm_content=pqfzh7y"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="rights flex-row">
+      <h3 class="text-gray">
+         Copyright ©2022 All rights reserved | made by
+         <a href="home.php" target="_black"><span>InLand Tours & Travels </span></a>
+         <a href="#" target="_black">(Alwyn Sebastian)</a>
+      </h3>
+   </div>
+   <div class="call">
+      <button class="btn form-btn"><a href="tel:+91-9847449044">Talk with our Travel Expert</a></button>
+   </div>
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
